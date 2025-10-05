eeb6ffa93de27585cc2000d91f66608332635524
SarveshSavaikar <sarveshsavaikar93@gmail.com>
2025-10-05 09:36:21 +0530

    REmoving the unecessory files

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,11 @@
-print("Hello")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "🚀 FastAPI is up and running!"}
+
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"greeting": f"Hello, {name}!"}
